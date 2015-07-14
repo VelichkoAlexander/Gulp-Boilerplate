ce0bddde3ca2bacff2ba45cf936742847a362263
Alexander   Velichko <alvi@mail.ua>
2015-07-14 11:31:18 +0300

    add new gilpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,90 +3,136 @@ var gulp = require('gulp'),
     rename = require("gulp-rename"),
     cache = require('gulp-cache'),
     notify = require('gulp-notify'),
-    livereload = require('gulp-livereload'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),// Компиляция SCSS
-    connect = require('gulp-connect'),
-    opn = require('opn'),
     wiredep = require('wiredep').stream,
-    minifyCSS=require('gulp-minify-css'),
-    autoprefixer=require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     concatCss = require('gulp-concat-css'),
+    browserSync = require('browser-sync').create(),//лайв-релоад
+    reload = browserSync.reload,//упрощение обращения к релоаду
+    plumber = require('gulp-plumber'),
+    notify = require("gulp-notify"),
     jade = require('gulp-jade'); // Компиляция Jade
 
-gulp.task('concatCss', function () {
-    gulp.src('app/css/*.css')
-        .pipe(concatCss("bundle.css"))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('out/'));
-});
-//local server config
-gulp.task('connect', function () {
-    connect.server({
-        root: ['app'],
-        livereload: true
+//===================================LIVERELOAD===================================
+
+// Static server
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
     });
-    opn('http://localhost:8080/');//  открывает браузер
+
 });
 
+//===================================END LIVERELOAD===================================
+
+//===================================HTML===================================
+
+//Собираем Jade
+gulp.task('jade', function () {
+    gulp.src(['app/jade/*.jade', '!app/jade/_*.jade'])	// Указываем какие файлы нужны
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(jade({									// Вызываем Jade
+            pretty: true								// Делаем красиво и богато, пока что.
+        }))
+        .pipe(gulp.dest('./app/'))						// Директория куда скидываются готовые файлы
+        // .pipe(notify("Jade ready!"))
+        .pipe(reload({stream: true}));				// Сервер перезапускаем
+
+});
+//html - при верстке без джейд
+gulp.task('html', function () {
+    gulp.src('app/*.html')
+        .pipe(notify("Html Complete!"))
+        .pipe(reload({stream: true}));
+});
+
+//===================================END HTML===================================
+
+//===================================CSS===================================
+
 //css
+
 gulp.task('css', function () {
     gulp.src('src/assets/styles/main.css')
         .pipe(rename('style.css'))
         .pipe(gulp.dest('built/css/'))
         .pipe(notify("Css ready!"))
-        .pipe(connect.reload());
+        .pipe(reload({stream: true}));
 });
 
-//Собираем Jade
-gulp.task('jade', function () {
-    gulp.src(['app/jade/*.jade', '!app/jade/_*.jade']) //Указываем какие файлы нужны
-        .pipe(jade({   								//Вызываем Jade
-            pretty: true 								// Делаем красиво и богато, пока что.
-        }))
-        .pipe(gulp.dest('./app/')) 					// Директория куда скидываются готовые файлы
-        .pipe(notify("Jade ready!"))
-        .pipe(connect.reload()); 						// Сервер перезапускаем
+//concatCss
+gulp.task('concatCss', function () {
+    gulp.src('./app/css/*.css')
+        .pipe(concatCss("bundle.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('out/css'));
 });
+
+//minify
+gulp.task('minify-css', function () {
+    return gulp.src('./app/css/*.css')
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
+});
+
 //Собираем SCSS
 gulp.task('sass', function () {
     gulp.src('app/scss/*.scss')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(sass({
-            errLogToConsole: true, // показывать ошибки в консоле
-            sync: true //для обработки больших файлов
+            errLogToConsole: true,						// показывать ошибки в консоле
+            sync: true									// для обработки больших файлов
         }))
-        //.pipe(minifyCSS())
+//.pipe(minifyCSS())
         .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
+            browsers: ['last 2 versions', 'Firefox ESR', 'Opera 12.1'],
             cascade: true
         }))
-        .pipe(gulp.dest('app/css/'))// Директория куда скидываются готовые файлы
-        .pipe(notify("Scss Complete!"))//Нотификация
-        .pipe(connect.reload()); // Сервер перезапускаем
+        .pipe(gulp.dest('app/css/'))					// Директория куда скидываются готовые файлы
+        // .pipe(notify("Scss Complete!"))				//Нотификация
+        .pipe(reload({stream: true}));					// Сервер перезапускаем
 });
 
-//html - при верстке без джейд
-gulp.task('html', function () {
-    gulp.src('app/*.html')
-        .pipe(notify("Html Complete!"))
-        .pipe(connect.reload());
-});
-//Js
+//===================================END CSS===================================
+
+//===================================JS===================================
+
+//reload Js
 gulp.task('js', function () {
     gulp.src('./app/js/*.js')
-        .pipe(notify("Js Complete!"))
-        .pipe(connect.reload());
+        // .pipe(notify("Js Complete!"))
+        .pipe(reload({stream: true}));
 });
+
+gulp.task('minify-js', function () {
+    gulp.src('./app/js/libs/**.js')
+        .pipe(uglify())
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('out/js/lib'));
+});
+
+//===================================END JS===================================
+
+//===================================BOWER===================================
+
 //wiredep
 gulp.task('wiredep', function () {
-    gulp.src('app/*.html')
+    gulp.src('app/jade/index.jade')
         .pipe(wiredep({
-            directory: 'app/components'
+            directory: 'app/components',
+            exclude: 'app/components/jquery'           //Исключаем ненужную папку
         }))
-        .pipe(gulp.dest('app'))
-        .pipe(notify("Bower include done!"))
-        .pipe(connect.reload());
-
+        .pipe(gulp.dest('app/jade/'))
+        .pipe(notify("wiredep include done!"))
 });
+
+//===================================END BOWER===================================
+
 //watcher
 gulp.task('watch', function () {
     gulp.watch('app/css/style.css', ['css']);
@@ -95,7 +141,33 @@ gulp.task('watch', function () {
     gulp.watch('app/jade/*.jade', ['jade']);
     gulp.watch('app/scss/**/_*.scss', ['sass']);
     gulp.watch('bower.json', ['wiredep']);
+
 });
 
 //default
-gulp.task('dev', ['connect', 'watch']);
+gulp.task('[dev]', ['browser-sync', 'watch']);
+
+gulp.task('[prod]', ['build']);
+
+
+gulp.task('build', function () {
+    gulp.src('./app/css/*.css')
+        .pipe(concatCss("bundle.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('out/css'));
+    gulp.src('./app/js/*.js')
+        .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('out/js'));
+    gulp.src('./app/*.html')
+        .pipe(gulp.dest('out/'));
+    gulp.src('./app/*.php')
+        .pipe(gulp.dest('out/'));
+    gulp.src('./app/fonts/**/*/')
+        .pipe(gulp.dest('out/fonts'));
+    gulp.src('./app/images/**/*/')
+        .pipe(gulp.dest('out/images'));
+    gulp.src('./app/js/libs/**/*/')
+        .pipe(gulp.dest('out/js/libs'));
+});
+
